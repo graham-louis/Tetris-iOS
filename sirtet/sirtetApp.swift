5651055760e09942559f7cac92baa020ffe163da
graham-louis <grahamlouis@ufl.edu>
2024-01-02 18:49:27 -0500

    Initial Commit

---
+++ sirtet/sirtetApp.swift
@@ -0,0 +1,17 @@
+//
+//  sirtetApp.swift
+//  sirtet
+//
+//  Created by Graham Louis on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct sirtetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
